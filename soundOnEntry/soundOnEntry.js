1900152f45e6dae7c8ac24f3716c2faa0b9da4fd
Fluffy Jenkins <richardjrn.w@gmail.com>
2018-12-18 00:22:59 +0000

    First commit of soundOnEntry

---
+++ soundOnEntry/soundOnEntry.js
@@ -0,0 +1,33 @@
+(function () {
+    var _entityID = null;
+    var userData;
+
+    function getUserData(entID) {
+        try {
+            userData = JSON.parse(Entities.getEntityProperties(entID).userData);
+        } catch (e) {
+            /**/
+        }
+    }
+
+    this.preload = function (entityID) {
+        _entityID = entityID;
+        getUserData(_entityID);
+    };
+
+    function playSound() {
+        if (userData.soundURL) {
+            Audio.playSound(SoundCache.getSound(userData.soundURL), {
+                orientation: MyAvatar.orientation,
+                position: MyAvatar.position,
+                volume: userData.volume ? userData.volume : 0.3,
+                pitch: 1,
+                localOnly: true
+            });
+        }
+    }
+
+    this.enterEntity = function () {
+        playSound();
+    };
+});
